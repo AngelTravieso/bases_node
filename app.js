59f3de63848b90e05a9b1bac00ab759f420b1355
Angel Travieso <angeltraviesoc@gmail.com>
2022-04-26 11:58:39 -0400

    configuración yargs independientes

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 // Importar modulo
+const { option } = require('yargs');
 const { crearArchivo } = require('./helpers/multiplicar');
 
 // yargs
-const argv = require('yargs').argv;
+const argv = require('yargs')
+            .option('b', {
+                alias: 'base',
+                type: 'number',
+                demandOption: true
+            })
+            .option('l', {
+                alias: 'listar',
+                type: 'boolean',
+                default: false,
+            })
+            .check( ( argv, option ) =>{
+                if( isNaN( argv.b ) ) {
+                    throw 'La base tiene que ser un número';
+                }
+
+                return true;
+            })
+            .argv;
+
+// option('l)
+// listar
+// boolean
+// default: false
+
 
 
 // Limpiar consola
@@ -10,20 +35,20 @@ console.clear();
 
 // Capturar parametros recibidos por consola (node)
 // console.log(process.argv);
-// console.log(argv);
+console.log(argv);
 
 // Capturar parametros recibidos por consola (yargs)
-console.log('base: yargs', argv.base);
+// console.log('base: yargs', argv.b );
 
 // Obtener argumentos de linea de comandos
 // const [,,arg3 = 'base=5'] = process.argv;
-// const [,base = 5] = arg3.split('=');
+// const [,base = 5] = arg3.split('='); 
 
 // const base = 5;
 
-// crearArchivo( base )
-//     .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
-//     .catch( err => console.log(err ) )
+crearArchivo( argv.b, argv.l )
+    .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
+    .catch( err => console.log(err ) )
 
 
 
--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -3,12 +3,9 @@ const fs = require('fs');
 // ES module (forma nueva)
 // import { fs } from 'fs';
 
-const crearArchivo = async( base = 5) => {
+const crearArchivo = async( base = 5, listar = false ) => {
     
     try {
-        console.log('===============================');
-        console.log(`     Tabla del ${ base }       `);
-        console.log('===============================');
 
         let salida = '';
 
@@ -16,7 +13,12 @@ const crearArchivo = async( base = 5) => {
             salida += `${ base } x ${ i } = ${ base * i } \n`;
         }
 
-        console.log(salida);
+        if( listar ) {
+            console.log('===============================');
+            console.log(`     Tabla del ${ base }     `);
+            console.log('===============================');
+            console.log(salida);
+        }
 
         fs.writeFileSync(`tabla-${ base }.txt`, salida );
         return `tabla-${ base }.txt`;
