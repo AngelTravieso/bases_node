9b21c3955959315599c4cea51b17220313ec863d
Angel Travieso <angeltraviesoc@gmail.com>
2022-04-26 09:54:48 -0400

    introducción yargs

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 // Importar modulo
 const { crearArchivo } = require('./helpers/multiplicar');
 
+// yargs
+const argv = require('yargs').argv;
+
 
 // Limpiar consola
 console.clear();
 
+// Capturar parametros recibidos por consola (node)
+// console.log(process.argv);
+// console.log(argv);
+
+// Capturar parametros recibidos por consola (yargs)
+console.log('base: yargs', argv.base);
+
 // Obtener argumentos de linea de comandos
-const [,,arg3 = 'base=5'] = process.argv;
-const [,base = 5] = arg3.split('=');
+// const [,,arg3 = 'base=5'] = process.argv;
+// const [,base = 5] = arg3.split('=');
 
 // const base = 5;
 
-crearArchivo( base )
-    .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
-    .catch( err => console.log(err ) )
+// crearArchivo( base )
+//     .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
+//     .catch( err => console.log(err ) )
 
 
 
