2118f02fb6f35406f5d8e35a09299a13f2c2e8fc
AngelTravieso <angeltraviesoc@gmail.com>
2022-04-26 00:13:08 -0400

    recibir información desde linea de comandos

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const { crearArchivo } = require('./helpers/multiplicar');
 // Limpiar consola
 console.clear();
 
-const base = 5;
+// Obtener argumentos de linea de comandos
+const [,,arg3 = 'base=5'] = process.argv;
+const [,base = 5] = arg3.split('=');
+
+// const base = 5;
 
 crearArchivo( base )
     .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
